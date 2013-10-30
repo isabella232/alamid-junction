950439e414ce891799b7690b803a374276e1ccd2
Johannes Ewald <johannes.ewald@peerigon.com>
2013-10-30 14:57:04 +0100

    - implemented dispose()-method

--- test/Junction.test.js
+++ test/Junction.test.js
@@ -1,6 +1,7 @@
 "use strict";
 
 var chai = require("chai"),
+    sinon = require("sinon"),
     Junction = require("../" + require("../package.json").main),
     AlamidSignal = require("alamid-signal"),
     expect = chai.expect;
@@ -8,162 +9,185 @@ var chai = require("chai"),
 chai.use(require("sinon-chai"));
 
 describe("Junction", function () {
-    var junction,
-        givenValue;
+    var junction;
 
-    function Signal() {
-        function signal(value) {
-            if (arguments.length === 1) {
-                givenValue = value;
-                return signal;
-            } else {
-                return givenValue;
-            }
-        }
-        signal.constructor = Signal;
-
-        return signal;
-    }
-
-    before(function () {
-        Junction.prototype.Signal = Signal;
-    });
     beforeEach(function () {
         junction = new Junction();
-    });
-    after(function () {
-        delete Junction.prototype.Signal;
     });
 
     it("should be an instance of Junction", function () {
         expect(junction).to.be.an.instanceof(Junction);
     });
 
-    describe(".get() before data has been set", function () {
+    describe(".prototype", function () {
+        var givenValue;
 
-        it("should return an empty object", function () {
-            expect(junction.get()).to.be.empty;
-        });
+        function Signal() {
+            function signal(value) {
+                if (arguments.length === 1) {
+                    givenValue = value;
+                    return signal;
+                } else {
+                    return givenValue;
+                }
+            }
+            signal.constructor = Signal;
 
-    });
-
-    describe(".get(key) before data has been set", function () {
-
-        it("should return undefined", function () {
-            expect(junction.get("name")).to.be.undefined;
-        });
-
-    });
-
-    describe(".set(object)", function () {
-
-        it("should return this", function () {
-            expect(junction.set({ type: "Pirate" })).to.equal(junction);
-        });
-
-    });
-
-    describe(".set(key, value)", function () {
-
-        it("should return this", function () {
-            expect(junction.set("hello", true)).to.equal(junction);
-        });
-
-    });
-
-    describe(".get() after data has been set", function () {
-        var data;
-
-        beforeEach(function () {
-            data = {
-                greeting: "Ahoy!",
-                age: 34,
-                attributes: {}
-            };
-            junction.set(data);
-        });
-
-        it("should return all data set", function () {
-            expect(junction.get()).to.deep.equal(data);
-
-            data.type = "Pirate";
-            data.greeting = "Arr!";
-            junction.set(data);
-
-            expect(junction.get()).to.deep.equal(data);
-        });
-
-    });
-
-    describe(".get(key) after data has been set", function () {
-
-        beforeEach(function () {
-            junction.set("greeting", "Ahoy!");
-            junction.set("age", 34);
-        });
-
-        it("should return the stored value", function () {
-            expect(junction.get("greeting")).to.equal("Ahoy!");
-            expect(junction.get("age")).to.equal(34);
-        });
-
-        it("should still return undefined for unset keys", function () {
-            expect(junction.get("victims")).to.equal(undefined);
-        });
-
-    });
-
-    describe(".signal(key)", function () {
-
-        it("should return a signal", function () {
-            var instance = junction.signal("greeting");
-
-            expect(instance).to.be.an("function");
-            expect(instance.constructor).to.equal(Signal);
-        });
-
-        it("should return the same signal instance multiple times", function () {
-            var instance;
-
-            instance = junction.signal("greeting");
-            expect(junction.signal("greeting")).to.equal(instance);
-        });
-
-        it("should return a signal representing the value specified by key", function () {
-            var greeting = junction.signal("greeting");
-
-            expect(greeting()).to.equal(undefined);
-            junction.set("greeting", "Ahoy!");
-            expect(greeting()).to.equal("Ahoy!");
-            expect(junction.get("greeting")).to.equal("Ahoy!");
-        });
-
-    });
-
-    describe(".signal(key) with alamid-signal", function () {
+            return signal;
+        }
 
         before(function () {
-            Junction.prototype.Signal = AlamidSignal;
-        });
-
-        after(function () {
             Junction.prototype.Signal = Signal;
         });
 
-        it("should return the same signal instance multiple times", function () {
-            var instance;
-
-            instance = junction.signal("greeting");
-            expect(junction.signal("greeting")).to.equal(instance);
+        after(function () {
+            delete Junction.prototype.Signal;
         });
 
-        it("should return a signal representing the value specified by key", function () {
-            var greeting = junction.signal("greeting");
+        describe(".get() before data has been set", function () {
 
-            expect(greeting()).to.equal(undefined);
-            junction.set("greeting", "Ahoy!");
-            expect(greeting()).to.equal("Ahoy!");
-            expect(junction.get("greeting")).to.equal("Ahoy!");
+            it("should return an empty object", function () {
+                expect(junction.get()).to.be.empty;
+            });
+
+        });
+
+        describe(".get(key) before data has been set", function () {
+
+            it("should return undefined", function () {
+                expect(junction.get("name")).to.be.undefined;
+            });
+
+        });
+
+        describe(".set(object)", function () {
+
+            it("should return this", function () {
+                expect(junction.set({ type: "Pirate" })).to.equal(junction);
+            });
+
+        });
+
+        describe(".set(key, value)", function () {
+
+            it("should return this", function () {
+                expect(junction.set("hello", true)).to.equal(junction);
+            });
+
+        });
+
+        describe(".get() after data has been set", function () {
+            var data;
+
+            beforeEach(function () {
+                data = {
+                    greeting: "Ahoy!",
+                    age: 34,
+                    attributes: {}
+                };
+                junction.set(data);
+            });
+
+            it("should return all data set", function () {
+                expect(junction.get()).to.deep.equal(data);
+
+                data.type = "Pirate";
+                data.greeting = "Arr!";
+                junction.set(data);
+
+                expect(junction.get()).to.deep.equal(data);
+            });
+
+        });
+
+        describe(".get(key) after data has been set", function () {
+
+            beforeEach(function () {
+                junction.set("greeting", "Ahoy!");
+                junction.set("age", 34);
+            });
+
+            it("should return the stored value", function () {
+                expect(junction.get("greeting")).to.equal("Ahoy!");
+                expect(junction.get("age")).to.equal(34);
+            });
+
+            it("should still return undefined for unset keys", function () {
+                expect(junction.get("victims")).to.equal(undefined);
+            });
+
+        });
+
+        describe(".signal(key)", function () {
+
+            it("should return a signal", function () {
+                var instance = junction.signal("greeting");
+
+                expect(instance).to.be.an("function");
+                expect(instance.constructor).to.equal(Signal);
+            });
+
+            it("should return the same signal instance multiple times", function () {
+                var instance;
+
+                instance = junction.signal("greeting");
+                expect(junction.signal("greeting")).to.equal(instance);
+            });
+
+            it("should return a signal representing the value specified by key", function () {
+                var greeting = junction.signal("greeting");
+
+                expect(greeting()).to.equal(undefined);
+                junction.set("greeting", "Ahoy!");
+                expect(greeting()).to.equal("Ahoy!");
+                expect(junction.get("greeting")).to.equal("Ahoy!");
+            });
+
+        });
+
+        describe(".signal(key) with alamid-signal", function () {
+
+            before(function () {
+                Junction.prototype.Signal = AlamidSignal;
+            });
+
+            after(function () {
+                Junction.prototype.Signal = Signal;
+            });
+
+            it("should return the same signal instance multiple times", function () {
+                var instance;
+
+                instance = junction.signal("greeting");
+                expect(junction.signal("greeting")).to.equal(instance);
+            });
+
+            it("should return a signal representing the value specified by key", function () {
+                var greeting = junction.signal("greeting");
+
+                expect(greeting()).to.equal(undefined);
+                junction.set("greeting", "Ahoy!");
+                expect(greeting()).to.equal("Ahoy!");
+                expect(junction.get("greeting")).to.equal("Ahoy!");
+            });
+
+        });
+
+        describe(".dispose()", function () {
+
+            it("should call dispose() on all signals that have been created by the junction", function () {
+                var name = junction.signal("name"),
+                    greeting = junction.signal("greeting");
+
+                name.dispose = sinon.spy();
+                greeting.dispose = sinon.spy();
+
+                junction.dispose();
+
+                expect(name.dispose).to.have.been.called;
+                expect(greeting.dispose).to.have.been.called;
+            });
+
         });
 
     });
